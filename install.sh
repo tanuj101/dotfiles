2e9bdbcee5862c8d97be2c9b1e8a3ed0592f2411
tanuj <tanujnagpal999@gmail.com>
2019-03-03 12:32:09 +0530

    latest commit

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install i3lock xss-lock xautolock scrot amixer light feh
-cp i3/config ~/.config/i3
-cp alacritty/alacritty.yml ~/.config/alacritty/
-sudo cp i3/lock_and_blur.sh /usr/local/bin/lock_and_blur.sh
-

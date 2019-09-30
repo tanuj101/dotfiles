829e15ce38dcc0e06fdd9bcb1b5860f8642a8bde
Tanuj Nagpal <tanujnagpal999@gmail.com>
2019-09-30 23:26:03 +0530

    Update and rename high_battery.sh to battery_alerts.sh

--- .scripts/notifications/battery_alerts.sh
+++ .scripts/notifications/battery_alerts.sh
@@ -4,3 +4,5 @@ if [ -r "$HOME/.dbus/Xdbus"  ]; then
 fi
 export DISPLAY=:0
 acpi --battery | awk -F, '/Charging/ { if (int($2) > 83) print  }' | xargs -ri /usr/bin/notify-send -u critical "Battery above 80!"
+acpi --battery | awk -F, '/Discharging/ { if (int($2) < 64) print  }' | xargs -ri /usr/bin/notify-send -u critical "Battery below 60!"
+
